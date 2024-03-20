3ad6c04d0a30eff54833cb10715989ffad65b77d
taylortien-99 <taylortien99@gmail.com>
2024-03-20 16:56:12 +0700

    update

--- src/migrations/insertUser.ts
+++ src/migrations/insertUser.ts
@@ -1,16 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
+import * as bcrypt from 'bcrypt';
 
 export class insertUser080100_1647629357376 implements MigrationInterface {
   name = 'insertUser080100_1647629357376';
   transaction?: boolean;
   async up(queryRunner: QueryRunner): Promise<void> {
+    const hashedPassword = await bcrypt.hash('111111111', 12);
     await queryRunner.query(
-      `INSERT INTO user (fullName, shortName, email, password, phone , role) VALUES
-      ('User 1 Full Name', 'User 1 Short Name', 'user1@example.com', 'password1', '123456789', 1),
-      ('User 2 Full Name', 'User 2 Short Name', 'user2@example.com', 'password2', '987654321', 1),
-      ('User 3 Full Name', 'User 3 Short Name', 'user3@example.com', 'password3', '456123789', 1),
-      ('User 4 Full Name', 'User 4 Short Name', 'user4@example.com', 'password4', '789456123', 1),
-      ('User 5 Full Name', 'User 5 Short Name', 'user5@example.com', 'password5', '321654987', 1)
+      `INSERT INTO user (fullName, shortName, email, password, phone , role, isFrozen) VALUES
+      ('Admin', 'admin', 'admin@gmail.com', '${hashedPassword}', '0123456789', 0, false),
+      ('Manager', 'manager', 'manager@gmail.com', '${hashedPassword}', '0123456789', 1, false),
+      ('Leader', 'leader', 'leader@gmail.com', '${hashedPassword}', '0123456789', 2, false),
+      ('Staff', 'staff', 'staff@gmail.com', '${hashedPassword}', '0123456789', 3, false)
     ON DUPLICATE KEY UPDATE email = VALUES(email)
    `,
     );
